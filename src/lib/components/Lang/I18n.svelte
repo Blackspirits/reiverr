8bcb0c2cf0f3c52a967085deb3948d033dc17aa4
Blackspirits <blackspirits@gmail.com>
2025-02-13 23:38:51 +0100

    Update I18n.svelte

--- src/lib/components/Lang/I18n.svelte
+++ src/lib/components/Lang/I18n.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { settings } from '$lib/stores/settings.store';
 	import { addMessages, init, locale } from 'svelte-i18n';
 
 	import de from '../../lang/de.json';
@@ -7,12 +6,17 @@
 	import es from '../../lang/es.json';
 	import fr from '../../lang/fr.json';
 	import it from '../../lang/it.json';
+	import ptPT from '../../lang/pt-PT.json';
+	import ptBR from '../../lang/pt-BR.json';
+	import { settings } from '../../stores/settings.store';
 
 	addMessages('de', de);
 	addMessages('en', en);
 	addMessages('es', es);
 	addMessages('fr', fr);
 	addMessages('it', it);
+	addMessages('pt_PT', ptPT);
+	addMessages('pt_BR', ptBR);
 
 	settings.subscribe((value) => {
 		if (value.language) {
